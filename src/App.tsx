f7f52f596e5c67d750587bec3f248aab71c67f45
Sumon Chandra <80484412+sumon-chandra@users.noreply.github.com>
2025-07-03 00:01:01 +0600

    Stater Templete

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const RootApp = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
+    </div>
+  );
+};
+
+export default RootApp;
